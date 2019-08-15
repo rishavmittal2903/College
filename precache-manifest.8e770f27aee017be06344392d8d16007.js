e96f0802e1681f0004a7289e3173fd6175b1ea43
H301816 <H301816@IE3BLT3BXYNH2>
2019-08-15 20:20:59 +0530

    Updates

---
+++ precache-manifest.8e770f27aee017be06344392d8d16007.js
@@ -0,0 +1,486 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa862f00d7e6ff1a1319a953406d2955",
+    "url": "/College/index.html"
+  },
+  {
+    "revision": "5370ee625b12963a7a6b",
+    "url": "/College/static/css/main.d8b23bed.chunk.css"
+  },
+  {
+    "revision": "0b248c7acc27be116cb7",
+    "url": "/College/static/js/2.949f71fd.chunk.js"
+  },
+  {
+    "revision": "5370ee625b12963a7a6b",
+    "url": "/College/static/js/main.011ff31d.chunk.js"
+  },
+  {
+    "revision": "2d0674349c5c9a9ebc1e",
+    "url": "/College/static/js/runtime~main.6f66d6dc.js"
+  },
+  {
+    "revision": "50c38ad145bc4f1c34c96661dc9181ee",
+    "url": "/College/static/media/1.50c38ad1.jpg"
+  },
+  {
+    "revision": "7def45cbaf4d96944bafd1ca92f0d379",
+    "url": "/College/static/media/2.7def45cb.jpg"
+  },
+  {
+    "revision": "115d1aa2377c007467c9d76acf703c65",
+    "url": "/College/static/media/3.115d1aa2.jpg"
+  },
+  {
+    "revision": "9e0638fa8df5a7c3f66117b19dcba1f7",
+    "url": "/College/static/media/BackImg.9e0638fa.jpg"
+  },
+  {
+    "revision": "88932dadc42e1bba93b21a76de60ef7a",
+    "url": "/College/static/media/Montserrat-Bold.88932dad.ttf"
+  },
+  {
+    "revision": "781190aecb862fffe858d42b124658cc",
+    "url": "/College/static/media/Montserrat-BoldItalic.781190ae.ttf"
+  },
+  {
+    "revision": "6786546363c0261228fd66d68bbf27e9",
+    "url": "/College/static/media/Montserrat-Italic.67865463.ttf"
+  },
+  {
+    "revision": "100b38fa184634fc89bd07a84453992c",
+    "url": "/College/static/media/Montserrat-Light.100b38fa.ttf"
+  },
+  {
+    "revision": "a98626e1aef6ceba5dfc1ee7112e235a",
+    "url": "/College/static/media/Montserrat-Medium.a98626e1.ttf"
+  },
+  {
+    "revision": "9c46095118380d38f12e67c916b427f9",
+    "url": "/College/static/media/Montserrat-Regular.9c460951.ttf"
+  },
+  {
+    "revision": "c88cecbffad6d8e731fd95de49561ebd",
+    "url": "/College/static/media/Montserrat-SemiBold.c88cecbf.ttf"
+  },
+  {
+    "revision": "67e1f424910e3ff800f7fb16e6384117",
+    "url": "/College/static/media/RegistrationImg.67e1f424.jpg"
+  },
+  {
+    "revision": "91c2419a63178a80a33a9c96493a0b7a",
+    "url": "/College/static/media/bg1.91c2419a.jpg"
+  },
+  {
+    "revision": "75184f169a432c902942c32263b82c6b",
+    "url": "/College/static/media/bg3.75184f16.jpg"
+  },
+  {
+    "revision": "c82ef9ffcbb056222617cbba16edd5d9",
+    "url": "/College/static/media/blog1.c82ef9ff.jpg"
+  },
+  {
+    "revision": "8b33128b6ac7932b069e6a5b4bda6560",
+    "url": "/College/static/media/blog2.8b33128b.jpg"
+  },
+  {
+    "revision": "a546ff9670a6b517045e9a3341b3d736",
+    "url": "/College/static/media/blog3.a546ff96.jpg"
+  },
+  {
+    "revision": "cd3a12c613f62658b104693b28e59ba6",
+    "url": "/College/static/media/body-bg.cd3a12c6.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/College/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/College/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/College/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/College/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/College/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "2e4b74d2cc71141b0858ed4012a859f0",
+    "url": "/College/static/media/g1.2e4b74d2.jpg"
+  },
+  {
+    "revision": "d195788e8d09a092f3037d559e895463",
+    "url": "/College/static/media/g2.d195788e.jpg"
+  },
+  {
+    "revision": "233e05363bf44e6a26c49cb6cd81ab85",
+    "url": "/College/static/media/g3.233e0536.jpg"
+  },
+  {
+    "revision": "184dc6555fda73baad37d4a80690531b",
+    "url": "/College/static/media/g4.184dc655.jpg"
+  },
+  {
+    "revision": "c289c62c39f5e9db1067fe68747a82a5",
+    "url": "/College/static/media/g5.c289c62c.jpg"
+  },
+  {
+    "revision": "b89366f040aa8c935c9ba41ea1dd6935",
+    "url": "/College/static/media/g6.b89366f0.jpg"
+  },
+  {
+    "revision": "3f3263071ad3773d96fd06ec34ec8340",
+    "url": "/College/static/media/logo1.3f326307.png"
+  },
+  {
+    "revision": "186cae8091da578150d81958e217714a",
+    "url": "/College/static/media/montserrat-v12-latin-900.186cae80.woff"
+  },
+  {
+    "revision": "260c2ea3ef57feb82251952e605a36d5",
+    "url": "/College/static/media/montserrat-v12-latin-900.260c2ea3.woff2"
+  },
+  {
+    "revision": "3924e08e4c91163c1b79560740c23c04",
+    "url": "/College/static/media/montserrat-v12-latin-900.3924e08e.eot"
+  },
+  {
+    "revision": "56909ed21a2f87b2a3532038ab80ddc8",
+    "url": "/College/static/media/montserrat-v12-latin-900.56909ed2.ttf"
+  },
+  {
+    "revision": "6062000bfaeb082adb6b5876d158b3a2",
+    "url": "/College/static/media/montserrat-v12-latin-900.6062000b.svg"
+  },
+  {
+    "revision": "2e0847ba0b1e237882854e8dab9da109",
+    "url": "/College/static/media/poppins-v5-latin-300.2e0847ba.eot"
+  },
+  {
+    "revision": "65bbf50d80ffbd75536abea87a4962db",
+    "url": "/College/static/media/poppins-v5-latin-300.65bbf50d.ttf"
+  },
+  {
+    "revision": "70e4020c8c2da472b5ce6ad4c68873d5",
+    "url": "/College/static/media/poppins-v5-latin-300.70e4020c.svg"
+  },
+  {
+    "revision": "dad8b32d6402d45efbd9f2a8ee6f203a",
+    "url": "/College/static/media/poppins-v5-latin-300.dad8b32d.woff"
+  },
+  {
+    "revision": "f6ef809fb833dbf1abcd5b35ab1576e5",
+    "url": "/College/static/media/poppins-v5-latin-300.f6ef809f.woff2"
+  },
+  {
+    "revision": "00b670ebdb4b1c61f8810fae255cb963",
+    "url": "/College/static/media/poppins-v5-latin-300italic.00b670eb.ttf"
+  },
+  {
+    "revision": "5e4a0e72a17dffda2e8c2af3af358110",
+    "url": "/College/static/media/poppins-v5-latin-300italic.5e4a0e72.woff2"
+  },
+  {
+    "revision": "81f9f9329c89f478b8f3cf8bc58f4c5c",
+    "url": "/College/static/media/poppins-v5-latin-300italic.81f9f932.eot"
+  },
+  {
+    "revision": "be5740135961833d443d22498c11df0a",
+    "url": "/College/static/media/poppins-v5-latin-300italic.be574013.svg"
+  },
+  {
+    "revision": "ffcf1f1c66a6661dabcf9a52581186d5",
+    "url": "/College/static/media/poppins-v5-latin-300italic.ffcf1f1c.woff"
+  },
+  {
+    "revision": "0261e08bd22d9f91c1d277cd4874ec95",
+    "url": "/College/static/media/poppins-v5-latin-500.0261e08b.woff"
+  },
+  {
+    "revision": "08609a017d830988630ee1b38a7ef71a",
+    "url": "/College/static/media/poppins-v5-latin-500.08609a01.woff2"
+  },
+  {
+    "revision": "12a5a771441907d9570020c1717158c3",
+    "url": "/College/static/media/poppins-v5-latin-500.12a5a771.eot"
+  },
+  {
+    "revision": "22b7688b2941298f2afd730763ef61d9",
+    "url": "/College/static/media/poppins-v5-latin-500.22b7688b.ttf"
+  },
+  {
+    "revision": "70e9051c6ebce0ffe149de16230903bf",
+    "url": "/College/static/media/poppins-v5-latin-500.70e9051c.svg"
+  },
+  {
+    "revision": "42b57730bd80b5048aaf027ea9009b51",
+    "url": "/College/static/media/poppins-v5-latin-500italic.42b57730.svg"
+  },
+  {
+    "revision": "7aca43b708f8dde583fbaa9bbbf8e1f5",
+    "url": "/College/static/media/poppins-v5-latin-500italic.7aca43b7.eot"
+  },
+  {
+    "revision": "9972bab7961b40e7da377f55c418e215",
+    "url": "/College/static/media/poppins-v5-latin-500italic.9972bab7.ttf"
+  },
+  {
+    "revision": "a43a4a36fcfe4d073146b54490eec662",
+    "url": "/College/static/media/poppins-v5-latin-500italic.a43a4a36.woff2"
+  },
+  {
+    "revision": "fabb1cda13707046efa0f7238555cc01",
+    "url": "/College/static/media/poppins-v5-latin-500italic.fabb1cda.woff"
+  },
+  {
+    "revision": "02faed9f2938bbba57514dfbea590d56",
+    "url": "/College/static/media/poppins-v5-latin-600.02faed9f.woff"
+  },
+  {
+    "revision": "437844ee60c122bea4bea58e74389280",
+    "url": "/College/static/media/poppins-v5-latin-600.437844ee.svg"
+  },
+  {
+    "revision": "693057428838523377f5c2da26f44899",
+    "url": "/College/static/media/poppins-v5-latin-600.69305742.eot"
+  },
+  {
+    "revision": "b01250b38f0ef1dc59107894d37711ec",
+    "url": "/College/static/media/poppins-v5-latin-600.b01250b3.ttf"
+  },
+  {
+    "revision": "d01ac68c4747e41a359cd94cecc29f9f",
+    "url": "/College/static/media/poppins-v5-latin-600.d01ac68c.woff2"
+  },
+  {
+    "revision": "319537c2e34754b8482dc10b5a5a678f",
+    "url": "/College/static/media/poppins-v5-latin-700.319537c2.eot"
+  },
+  {
+    "revision": "44bb1bc773b816dfb13a88f43d0d0476",
+    "url": "/College/static/media/poppins-v5-latin-700.44bb1bc7.ttf"
+  },
+  {
+    "revision": "82b5580c33900775f45b395686110d38",
+    "url": "/College/static/media/poppins-v5-latin-700.82b5580c.svg"
+  },
+  {
+    "revision": "d1d7b85dc76bad1a967fc842d110262c",
+    "url": "/College/static/media/poppins-v5-latin-700.d1d7b85d.woff"
+  },
+  {
+    "revision": "d9f0e4b31953d6440559059230442358",
+    "url": "/College/static/media/poppins-v5-latin-700.d9f0e4b3.woff2"
+  },
+  {
+    "revision": "0470693a3857401dad7c0ca10db65617",
+    "url": "/College/static/media/poppins-v5-latin-700italic.0470693a.ttf"
+  },
+  {
+    "revision": "6a428f7f4ebfe39102f6c30421b9ace0",
+    "url": "/College/static/media/poppins-v5-latin-700italic.6a428f7f.svg"
+  },
+  {
+    "revision": "d03f7611353f6f9e728b329d982b7660",
+    "url": "/College/static/media/poppins-v5-latin-700italic.d03f7611.woff"
+  },
+  {
+    "revision": "da80a603d0220f8e5bb3aaf0e5b42763",
+    "url": "/College/static/media/poppins-v5-latin-700italic.da80a603.woff2"
+  },
+  {
+    "revision": "ef75019674fb2adad506e031bd14331e",
+    "url": "/College/static/media/poppins-v5-latin-700italic.ef750196.eot"
+  },
+  {
+    "revision": "1430c339df4a2dd02998eac6dc591098",
+    "url": "/College/static/media/poppins-v5-latin-800.1430c339.svg"
+  },
+  {
+    "revision": "2f8f4d43aac53d1b881fcd50b08ac5e8",
+    "url": "/College/static/media/poppins-v5-latin-800.2f8f4d43.eot"
+  },
+  {
+    "revision": "6f22cd6faf712f6973ee0f38cd38c0e6",
+    "url": "/College/static/media/poppins-v5-latin-800.6f22cd6f.woff2"
+  },
+  {
+    "revision": "97ae8f0f4c3d743a27496689665416bc",
+    "url": "/College/static/media/poppins-v5-latin-800.97ae8f0f.ttf"
+  },
+  {
+    "revision": "99a9e7bac31ddfc2b58e0025f76c65d1",
+    "url": "/College/static/media/poppins-v5-latin-800.99a9e7ba.woff"
+  },
+  {
+    "revision": "2908d7aad95f5fe15d373056e80efc9a",
+    "url": "/College/static/media/poppins-v5-latin-800italic.2908d7aa.svg"
+  },
+  {
+    "revision": "5c8a310a6164fe0dfec074a89d94f181",
+    "url": "/College/static/media/poppins-v5-latin-800italic.5c8a310a.woff"
+  },
+  {
+    "revision": "acbf8d91de92514c5111f02cb26854a9",
+    "url": "/College/static/media/poppins-v5-latin-800italic.acbf8d91.woff2"
+  },
+  {
+    "revision": "cc33beefca3fdaae55ac852f32278166",
+    "url": "/College/static/media/poppins-v5-latin-800italic.cc33beef.eot"
+  },
+  {
+    "revision": "e85ba4fc01dc62bf49b51b337b710ca8",
+    "url": "/College/static/media/poppins-v5-latin-800italic.e85ba4fc.ttf"
+  },
+  {
+    "revision": "11c0d35b23006eefd198bb52e3e617f9",
+    "url": "/College/static/media/poppins-v5-latin-900.11c0d35b.ttf"
+  },
+  {
+    "revision": "1a6c24bd1739f450184ffb0da3c1d2a3",
+    "url": "/College/static/media/poppins-v5-latin-900.1a6c24bd.svg"
+  },
+  {
+    "revision": "1b8eddbe62c11ea9eb4f407ecd897827",
+    "url": "/College/static/media/poppins-v5-latin-900.1b8eddbe.woff"
+  },
+  {
+    "revision": "480a646050fb8b991ebff5543ee756b0",
+    "url": "/College/static/media/poppins-v5-latin-900.480a6460.eot"
+  },
+  {
+    "revision": "f53d38d7a9ce9fb32f278410609cd7d4",
+    "url": "/College/static/media/poppins-v5-latin-900.f53d38d7.woff2"
+  },
+  {
+    "revision": "055f0699f59e8b1eb28270b304c5d11b",
+    "url": "/College/static/media/poppins-v5-latin-italic.055f0699.woff"
+  },
+  {
+    "revision": "40f3c34fe1de3154a60c3af7d1d251e6",
+    "url": "/College/static/media/poppins-v5-latin-italic.40f3c34f.svg"
+  },
+  {
+    "revision": "506db506cbae0183e1b2c50d069d3769",
+    "url": "/College/static/media/poppins-v5-latin-italic.506db506.ttf"
+  },
+  {
+    "revision": "936aaefc2efb764f3f52c0e9f34ef749",
+    "url": "/College/static/media/poppins-v5-latin-italic.936aaefc.woff2"
+  },
+  {
+    "revision": "9741803691bc57d55082d53237380709",
+    "url": "/College/static/media/poppins-v5-latin-italic.97418036.eot"
+  },
+  {
+    "revision": "1fce830e6112511a77108832e13172fd",
+    "url": "/College/static/media/poppins-v5-latin-regular.1fce830e.woff"
+  },
+  {
+    "revision": "4711bbc2872e5e734fe5d84804f24967",
+    "url": "/College/static/media/poppins-v5-latin-regular.4711bbc2.ttf"
+  },
+  {
+    "revision": "74955fcba4d3cb051de366faff68660b",
+    "url": "/College/static/media/poppins-v5-latin-regular.74955fcb.svg"
+  },
+  {
+    "revision": "ce0c9ae08840a0b43bccb9f5a86e155d",
+    "url": "/College/static/media/poppins-v5-latin-regular.ce0c9ae0.woff2"
+  },
+  {
+    "revision": "f00ab8be77768ee0afe76d8fa105e7ee",
+    "url": "/College/static/media/poppins-v5-latin-regular.f00ab8be.eot"
+  },
+  {
+    "revision": "00e7dda395395be0db824d6155cf0a71",
+    "url": "/College/static/media/roboto-slab-v7-latin-300.00e7dda3.eot"
+  },
+  {
+    "revision": "26f151c74f2c0a6efc1683965a2f26ec",
+    "url": "/College/static/media/roboto-slab-v7-latin-300.26f151c7.ttf"
+  },
+  {
+    "revision": "8d67ae81f4ebe19519b0f438460b7829",
+    "url": "/College/static/media/roboto-slab-v7-latin-300.8d67ae81.svg"
+  },
+  {
+    "revision": "9b8e3dd80d4ba8b9877cd99cec8e7437",
+    "url": "/College/static/media/roboto-slab-v7-latin-300.9b8e3dd8.woff2"
+  },
+  {
+    "revision": "c0209ea1a46d7e74fc666e81d8d2b7d9",
+    "url": "/College/static/media/roboto-slab-v7-latin-300.c0209ea1.woff"
+  },
+  {
+    "revision": "3a3fd2bb95d56d2f6fa63791d869fcf4",
+    "url": "/College/static/media/roboto-slab-v7-latin-700.3a3fd2bb.ttf"
+  },
+  {
+    "revision": "6f7d5987daa50748c2ac423a9ad6adad",
+    "url": "/College/static/media/roboto-slab-v7-latin-700.6f7d5987.woff2"
+  },
+  {
+    "revision": "b234688308244e1918ae61ae78a79a91",
+    "url": "/College/static/media/roboto-slab-v7-latin-700.b2346883.svg"
+  },
+  {
+    "revision": "d7eb60ce429506ac0f00c5773b97d2c6",
+    "url": "/College/static/media/roboto-slab-v7-latin-700.d7eb60ce.woff"
+  },
+  {
+    "revision": "fafe68ff4de2c30596a0d84665de712c",
+    "url": "/College/static/media/roboto-slab-v7-latin-700.fafe68ff.eot"
+  },
+  {
+    "revision": "1e09096ff6a997de7f52c3a7eadf7413",
+    "url": "/College/static/media/roboto-slab-v7-latin-regular.1e09096f.eot"
+  },
+  {
+    "revision": "3c3d09354e2028e0877ee77a48c0e8d7",
+    "url": "/College/static/media/roboto-slab-v7-latin-regular.3c3d0935.ttf"
+  },
+  {
+    "revision": "8817cc38b4c986fcf46c6a5a420a8321",
+    "url": "/College/static/media/roboto-slab-v7-latin-regular.8817cc38.svg"
+  },
+  {
+    "revision": "a33f5bf82e263ebbf148b6518563a8a6",
+    "url": "/College/static/media/roboto-slab-v7-latin-regular.a33f5bf8.woff"
+  },
+  {
+    "revision": "d75507313e9d71263646fc2ee4ae2af2",
+    "url": "/College/static/media/roboto-slab-v7-latin-regular.d7550731.woff2"
+  },
+  {
+    "revision": "75eb7c4e5d58a0a3f0d387b9bc44e080",
+    "url": "/College/static/media/signup-image.75eb7c4e.jpg"
+  },
+  {
+    "revision": "9de28a7ccd243c9c38ff6b20437bab07",
+    "url": "/College/static/media/t1.9de28a7c.jpg"
+  },
+  {
+    "revision": "8f957e4d375fc93ce5ca3aa1f01ba290",
+    "url": "/College/static/media/t2.8f957e4d.jpg"
+  },
+  {
+    "revision": "2c0e33eae10d6f90a8f28175b5ab7dd9",
+    "url": "/College/static/media/t3.2c0e33ea.jpg"
+  },
+  {
+    "revision": "9b5c068ca732966bb9a952070010370b",
+    "url": "/College/static/media/t4.9b5c068c.jpg"
+  },
+  {
+    "revision": "0e7bc0028dd83ff18e49e8744b80332d",
+    "url": "/College/static/media/who.0e7bc002.jpg"
+  }
+]);
